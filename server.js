168b5afc1417970d1032c3be85038be2037a3da0
nawifuru <96987973+nawifuru@users.noreply.github.com>
2022-01-05 23:00:50 +0800

    ssl

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const https = require('https');
+const path = require('path');
+const fs = require('fs');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const dataScraper = require('./dataScraper');
@@ -11,14 +14,18 @@ async function main() {
     await gamesRoute.Init();
     app.use('/games', gamesRoute.Router());
     app.use(bodyParser.json());
-    app.listen(port, err => {
+    const sslServer = https.createServer({
+        key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+        cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
+    }, app)
+    sslServer.listen(port, err => {
         if (err)
             console.log(err);
         console.log(`Server started.... Listening to port ${port}....`);
-    });
+    })
     // await dataScraper.UpdateApplist();
     // await dataScraper.UpdateAppDetails();
-    // await dataScraper.UpdateAppReviews(42127);
+    // await dataScraper.UpdateAppReviews(42138);
     // await dataScraper.UpdateAppMetrics();
 }
 main();
